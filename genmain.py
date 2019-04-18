145298444f0878186b31ab2ca50eef3132815515
L. Kärkkäinen <tronic@users.noreply.github.com>
2019-04-18 14:39:52 +0300

    More random hacking (and poor commit messages).

--- genmain.py
+++ genmain.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import torch
 import torch.nn as nn
+import time
 
 #%% Setup
 
@@ -54,7 +55,7 @@ class Discriminator(nn.Module):
         return self.seq(faces)
 
 # How many random numbers are fed to generator
-zdim = 10
+zdim = 100
 
 class Generator(nn.Module):
     """Convolutional generator adapted from DCGAN by Radford et al."""
@@ -64,8 +65,9 @@ class Generator(nn.Module):
         CH = 512
         self.seq = nn.Sequential(
             nn.Linear(zdim, CH * 4 * 4, bias=False),
-            nn.BatchNorm1d(CH * 4 * 4),
             Reshape(CH, 4, 4),
+            nn.BatchNorm2d(CH),
+            nn.Tanh(),
             nn.ConvTranspose2d(in_channels=CH, out_channels=CH // 2, kernel_size=4, stride=2, bias=False),
             nn.BatchNorm2d(CH // 2),
             nn.Tanh(),
@@ -103,7 +105,7 @@ generator.to(device)
 discriminator.to(device)
 
 #%% Training epochs
-d_optimizer = torch.optim.Adam(discriminator.parameters(), lr=0.001, betas=(.5, .999), weight_decay=1e-3)
+d_optimizer = torch.optim.Adam(discriminator.parameters(), lr=0.001, betas=(.5, .999), weight_decay=1e-4)
 g_optimizer = torch.optim.Adam(generator.parameters(), lr=0.0002, betas=(.5, .999))
 criterion = nn.BCEWithLogitsLoss()
 minibatch_size = 24
@@ -112,81 +114,81 @@ epochs = range(50)
 ones = torch.ones((minibatch_size, 1), device=device)
 zeros = torch.zeros((minibatch_size, 1), device=device)
 
-import time
-
-class Timer:
-    def __init__(self):
-        self.name = None
-        self.stats = {}
-    def __call__(self, name):
-        t = time.perf_counter()
-        if self.name:
-            self.stats[self.name] = self.stats.get(self.name, 0.0) + (t - self.t)
-        self.name = name
-        self.t = t
-    def dump(self):
-        print("Time spent:")
-        for n, t in self.stats.items():
-            print(f"{n:14s}{t:7.2f}")
-
 print(f"Training with {len(rounds)} rounds per epoch:")
-timer = Timer()
 for e in epochs:
     d_rounds = g_rounds = 0
     rtimer = time.perf_counter()
     for r in rounds:
-        print(f"  [{'*' * (30 * r // rounds[-1]):30s}] {r+1:4d}/{len(rounds)} {(time.perf_counter() - rtimer) / (r + .1) * len(rounds):4.0f} s/epoch", end="\r")
+        print(f"  [{'*' * (25 * r // rounds[-1]):25s}] {r+1:4d}/{len(rounds)} {(time.perf_counter() - rtimer) / (r + .1) * len(rounds):3.0f} s/epoch", end="\r")
         # Run the discriminator on real and fake data
-        timer("batch load")
         batch = slice(r * minibatch_size, (r + 1) * minibatch_size)
         real = images[batch].to(torch.float32)
         real /= 255.0
-        timer("update init")
         d_optimizer.zero_grad()
         z = torch.randn((minibatch_size, zdim), device=device)
-        timer("fake")
         fake = generator(z)
         output_fake = discriminator(fake.detach())
-        timer("real")
         output_real = discriminator(real)
-        # Check levels (only every few rounds because it is slow)
-        if r % 30 == 0 or r == rounds[-1]:
-            timer("levels")
-            realout = output_real.detach().cpu().numpy().reshape(minibatch_size)
-            fakeout = output_fake.detach().cpu().numpy().reshape(minibatch_size)
-            out = 1 / (1 + np.exp(-np.array([realout, fakeout])))
-            realout, fakeout = out.mean(axis=1)
-            diff = realout - fakeout
+        # Check levels
+        levels = np.stack((
+            output_real.detach().cpu().numpy().reshape(minibatch_size),
+            output_fake.detach().cpu().numpy().reshape(minibatch_size)
+        ))
+        levels = 1.0 / (1.0 + np.exp(-levels))  # Sigmoid for probability
+        level_real, level_fake = levels.mean(axis=1)
+        level_diff = level_real - level_fake
         # Train the discriminator only if it is not too good
-        if diff < 0.8:
-            timer("d update")
+        if level_diff < 0.6:
             criterion(output_real, ones).backward()
             criterion(output_fake, zeros).backward()
             d_optimizer.step()
             d_rounds += 1
         # Train the generator only if the discriminator works
-        if diff > 0.2:
-            timer("g update")
+        if level_diff > 0.4:
             g_optimizer.zero_grad()
             criterion(discriminator(fake), ones).backward()
             g_optimizer.step()
             g_rounds += 1
-        timer(None)
-    print(f"  Epoch {e+1:2d}/{len(epochs)}   {d_rounds:4d}×D {g_rounds:4d}×G » real {realout:3.0%} vs. fake {fakeout:3.0%}")
+    print(f"  Epoch {e+1:2d}/{len(epochs)}   {d_rounds:4d}×D {g_rounds:4d}×G » real {level_real:3.0%} vs. fake {level_fake:3.0%}")
     # Show the results
-    cols, rows = 4, 4  # minibatch_size
+    cols, rows = 4, 4  # no more than minibatch_size
     fig, axes = plt.subplots(rows, cols, figsize=(cols * 2, rows * 2))
+    #real = real.detach().cpu().permute(0, 2, 3, 1).numpy()
+    fake = fake.detach().cpu().permute(0, 2, 3, 1).numpy()
     for i, ax in enumerate(axes.flat):
-        ax.imshow(fake[i].detach().cpu().permute(1, 2, 0))
+        ax.imshow(fake[i])
         ax.grid(False)
         ax.set_yticklabels([])
         ax.set_xticklabels([])
-        ax.set_title(f"{out[1, i]:.0%} real")
+        ax.set_title(f"{levels[1, i]:.0%} real")
     plt.show()
-
-timer.dump()
 
 #%% Save current state
 torch.save(generator.state_dict(), "generator.pth")
 torch.save(discriminator.state_dict(), "discriminator.pth")
 print("Saved to generator.pth and discriminator.pth!")
+
+#%% Prepare CPU-based generator for evaluation
+gen = Generator()
+gen.load_state_dict(generator.state_dict())
+gen.eval()
+
+def mix(a, b, x):
+    return a * (1 - x) + b * x
+
+
+points = torch.randn((4, 1, zdim))
+cols, rows = 6, 6
+fig, axes = plt.subplots(rows, cols, figsize=(cols * 2, rows * 2))
+for x in range(cols):
+    ab = mix(points[0], points[1], x / cols)
+    cd = mix(points[2], points[3], x / cols)
+    for y in range(rows):
+        p = mix(ab, cd, y/rows)
+        fake = gen(p).detach().permute(0, 2, 3, 1).numpy()
+        ax = axes[y][x]
+        ax.imshow(fake[0])
+        ax.grid(False)
+        ax.set_yticklabels([])
+        ax.set_xticklabels([])
+plt.show()
