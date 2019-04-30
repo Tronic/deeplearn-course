c97fbb3b954624a64df810b68d6c32081e3ab891
L. Kärkkäinen <tronic@users.noreply.github.com>
2019-04-30 16:17:57 +0300

    Hacks to support visualization to PNGs, for training video.

--- genmain.py
+++ genmain.py
@@ -6,14 +6,6 @@ import time
 
 #%% Setup
 
-plots = False
-#plots = True
-
-if plots:
-    import matplotlib.pyplot as plt
-    import seaborn as sns
-    sns.set_style("darkgrid")  # Make pyplot look better
-
 def images_tensor(images, stride=2):
     """Scale, transpose and convert Numpy tensor into Torch tensor."""
     global device
@@ -25,6 +17,63 @@ print("Uploading tensors to", device)
 images = images_tensor(facedata.images)
 
 assert images.shape == torch.Size((facedata.N, 3, 100, 100))
+
+# How many random numbers are fed to generator
+zdim = 100
+
+def latent_tensor(*size):
+    """Create random latent variables for size samples."""
+    global zdim, device
+    z = torch.randn((*size, zdim), device=device)
+    lengths = (z * z).sum(dim=-1, keepdim=True)
+    return z / lengths * zdim  # Form a hypersphere of radius zdim
+
+
+#%% Visualization init
+
+visualization = "png"
+cols, rows = 4, 4
+
+if visualization == "plt":
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    sns.set_style("darkgrid")  # Make pyplot look better
+
+    def visualize():
+        global cols, rows, z_test, generator
+        fig, axes = plt.subplots(rows, cols, figsize=(cols * 2, rows * 2))
+        fake_test = generator(z_test)
+        levels_test = discriminator(fake_test).detach().cpu().numpy()
+        levels_test = 1.0 / (1.0 + np.exp(-levels_test))  # Sigmoid for probability
+
+        fake_test = fake_test.detach().cpu().permute(0, 2, 3, 1).numpy()
+        for i, ax in enumerate(axes.flat):
+            ax.imshow(fake_test[i] / 2 + .5)
+            ax.grid(False)
+            ax.set_yticklabels([])
+            ax.set_xticklabels([])
+            ax.set_title(f"{levels_test[i, 0]:.0%} real")
+        plt.show()
+elif visualization == "png":
+    from PIL import Image
+    cols, rows = 8, 5
+    counter = 0
+    def visualize():
+        global cols, rows, z_test, generator, counter
+        s = 100
+        fake_test = ((generator(z_test).detach() + 1.0) * 127.5).cpu().permute(0, 2, 3, 1).numpy()
+        img = np.empty((rows * s, cols * s, 3), dtype=np.uint8)
+        for r in range(rows):
+            for c in range(cols):
+                img[r * s : (r + 1) * s, c * s : (c + 1) * s, :] = fake_test[r * cols + c]
+        Image.fromarray(img).save(f"training/img{counter:05}.png")
+        counter += 1
+
+else:
+    def visualize():
+        pass
+
+z_test = latent_tensor(rows * cols)
 
 #%% Network definition
 
@@ -63,22 +112,12 @@ class Discriminator(nn.Module):
     def forward(self, faces):
         return self.seq(faces)
 
-# How many random numbers are fed to generator
-zdim = 100
-
-def random_latent(*size):
-    """Create random latent variables for size samples."""
-    global zdim, device
-    z = torch.randn((*size, zdim), device=device)
-    lengths = (z * z).sum(dim=-1, keepdim=True)  # Normalize to unit length (form a hypersphere)
-    return z / lengths
-
 class Generator(nn.Module):
     """Convolutional generator adapted from DCGAN by Radford et al."""
     def __init__(self):
         global zdim
         super().__init__()
-        CH = 512
+        CH = 1024
         self.seq = nn.Sequential(
             nn.Linear(zdim, CH * 4 * 4, bias=False),
             Reshape(CH, 4, 4),
@@ -127,17 +166,13 @@ except:
 generator.to(device)
 discriminator.to(device)
 
-#%% Test plot parameters
-cols, rows = 4, 4
-z_test = random_latent(rows * cols)
-
 #%% Training epochs
 d_optimizer = torch.optim.Adam(discriminator.parameters(), lr=0.001, betas=(.5, .999))
-g_optimizer = torch.optim.Adam(generator.parameters(), lr=0.0001, betas=(.5, .999))
+g_optimizer = torch.optim.Adam(generator.parameters(), lr=0.00005, betas=(.5, .998))
 criterion = nn.BCEWithLogitsLoss()
-minibatch_size = 24
+minibatch_size = 64
 rounds = range(facedata.N // minibatch_size if device.type == "cuda" else 10)
-epochs = range(50)
+epochs = range(60 * 300)
 ones = torch.ones((minibatch_size, 1), device=device)
 zeros = torch.zeros((minibatch_size, 1), device=device)
 
@@ -155,7 +190,7 @@ for e in epochs:
         real /= 128.0
         real -= 1.0
         d_optimizer.zero_grad()
-        z = random_latent(minibatch_size)
+        z = latent_tensor(minibatch_size)
         fake = generator(z)
         output_fake = discriminator(fake.detach())
         output_real = discriminator(real)
@@ -179,22 +214,10 @@ for e in epochs:
             criterion(discriminator(fake), ones).backward()
             g_optimizer.step()
             g_rounds += 1
+            visualize()
     print(f"  Epoch {e+1:2d}/{len(epochs)}   {d_rounds:4d}×D {g_rounds:4d}×G » real {level_real:3.0%} vs. fake {level_fake:3.0%}")
     # Show the results
-    if plots:
-        fig, axes = plt.subplots(rows, cols, figsize=(cols * 2, rows * 2))
-        fake_test = generator(z_test)
-        levels_test = discriminator(fake_test).detach().cpu().numpy()
-        levels_test = 1.0 / (1.0 + np.exp(-levels_test))  # Sigmoid for probability
-
-        fake_test = fake_test.detach().cpu().permute(0, 2, 3, 1).numpy()
-        for i, ax in enumerate(axes.flat):
-            ax.imshow(fake_test[i] / 2 + .5)
-            ax.grid(False)
-            ax.set_yticklabels([])
-            ax.set_xticklabels([])
-            ax.set_title(f"{levels_test[i, 0]:.0%} real")
-        plt.show()
+    #visualize()
 
 #%% Save current state
 torch.save(generator.state_dict(), "generator.pth")
