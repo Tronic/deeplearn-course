32217578e89fa756954e9e260ed8400a2be72a05
L. Kärkkäinen <tronic@users.noreply.github.com>
2019-05-18 11:58:09 +0300

    Bugfix

--- genmain.py
+++ genmain.py
@@ -126,8 +126,8 @@ class Generator(nn.Module):
             if train:
                 x0 = x.detach()
                 #x1 = torch.cat((x0[1:], x0[0:1]), dim=0)
-                xd = x0 - x0.mean(dim=1)
-                x.backward(xd.sign() / (abs(xd).mean(dim=1) + 0.001), retain_graph=True)
+                xd = x0 - x0.mean(dim=1, keepdim=True)
+                x.backward(xd.sign() / (abs(xd).mean(dim=1, keepdim=True) + 0.001), retain_graph=True)
         # Alpha blending between the last two layers
         if alpha < 1 and "x_prev" in locals():
             x_prev = nn.functional.interpolate(x_prev, scale_factor=2, mode="bilinear", align_corners=False)
