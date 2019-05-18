a5dfc39d5aa30031584ad8a2df2f0346f4359ed9
L. Kärkkäinen <tronic@users.noreply.github.com>
2019-05-18 11:06:51 +0300

    Variance-based

--- genmain.py
+++ genmain.py
@@ -125,9 +125,9 @@ class Generator(nn.Module):
             # Minimize correlation between samples
             if train:
                 x0 = x.detach()
-                x1 = torch.cat((x0[1:], x0[0:1]), dim=0)
-                xd = x0 - x1
-                x.backward(xd / (xd**2 + 1e-5), retain_graph=True)
+                #x1 = torch.cat((x0[1:], x0[0:1]), dim=0)
+                xd = x0 - x0.mean()
+                x.backward(xd / x0.var(), retain_graph=True)
         # Alpha blending between the last two layers
         if alpha < 1 and "x_prev" in locals():
             x_prev = nn.functional.interpolate(x_prev, scale_factor=2, mode="bilinear", align_corners=False)
