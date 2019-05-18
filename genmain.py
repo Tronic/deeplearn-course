e0225adfa946c2c8663ecddd9f6ae9c5642853bc
L. Kärkkäinen <tronic@users.noreply.github.com>
2019-05-18 18:00:53 +0300

    Make video 10 s per pass (less frames recorded).

--- genmain.py
+++ genmain.py
@@ -224,7 +224,7 @@ def training():
             bar = f"E{e} {image_size:3}px [{'»' * (20 * r // rounds[-1]):20s}] {r+1:04d}"
             alp = 2 * " ░▒▓█"[int(alpha * 4)]
             stats = f"α={alp} lr={glr*1e6:03.0f}µ ε={noise*1e3:03.0f}m {level_real:4.0%} vs{level_fake:4.0%}  {(time.perf_counter() - rtimer) / (r + .1) * len(rounds):3.0f} s/epoch"
-            visualize(f"{bar} {stats}", image_size=image_size, alpha=alpha)
+            if r % 5 == 0: visualize(f"{bar} {stats}", image_size=image_size, alpha=alpha)
             print(f"\r  {bar} {stats} ", end="\N{ESC}[K")
         # After an epoch is finished:
         print()
